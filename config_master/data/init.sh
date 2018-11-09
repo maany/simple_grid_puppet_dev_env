c41caa74e4a94409c7275a67acf7156e2f871f43
maany <imptodefeat@gmail.com>
2018-11-09 17:16:31 +0100

    add puppet module inside config_master

--- config_master/data/init.sh
+++ config_master/data/init.sh
@@ -2,6 +2,17 @@
 
 cp /data/puppetserver /etc/sysconfig/puppetserver
 cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
+
 hostnamectl set-hostname config_master.cern.ch
 hostnamectl set-hostname config_master.cern.ch
+
+echo "Creating symlinks for puppet module"
+ln -s /simple_grid_puppet_module /etc/puppetlabs/code/environments/install/site/simple_grid
+ln -s /simple_grid_puppet_module /etc/puppetlabs/code/environments/config/site/simple_grid
+ln -s /simple_grid_puppet_module /etc/puppetlabs/code/environments/pre_deploy/site/simple_grid
+ln -s /simple_grid_puppet_module /etc/puppetlabs/code/environments/deploy/site/simple_grid
+ln -s /simple_grid_puppet_module /etc/puppetlabs/code/environments/test/site/simple_grid
+ln -s /simple_grid_puppet_module /etc/puppetlabs/code/environments/cleanup/site/simple_grid
+echo "Starting Puppet Server"
 systemctl start puppetserver
+echo "All Set for development"
