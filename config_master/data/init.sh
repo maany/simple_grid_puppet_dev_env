df516576dc291e54f243869c2ebcd460096a1c72
maany <imptodefeat@gmail.com>
2018-11-09 18:15:12 +0100

    update symlink

--- config_master/data/init.sh
+++ config_master/data/init.sh
@@ -8,11 +8,11 @@ hostnamectl set-hostname config_master.cern.ch
 
 echo "Creating symlinks for puppet module"
 cp -r --symbolic-link /simple_grid/* /etc/puppetlabs/code/environments/install/site/simple_grid/
-cp -r --symbolic-link /simple_grid /etc/puppetlabs/code/environments/config/site/simple_grid/
-cp -r --symbolic-link /simple_grid /etc/puppetlabs/code/environments/pre_deploy/site/simple_grid/
-cp -r --symbolic-link /simple_grid /etc/puppetlabs/code/environments/deploy/site/simple_grid/
-cp -r --symbolic-link /simple_grid /etc/puppetlabs/code/environments/test/site/simple_grid/
-cp -r --symbolic-link /simple_grid /etc/puppetlabs/code/environments/cleanup/site/simple_grid/
+cp -r --symbolic-link /simple_grid/* /etc/puppetlabs/code/environments/config/site/simple_grid/
+cp -r --symbolic-link /simple_grid/* /etc/puppetlabs/code/environments/pre_deploy/site/simple_grid/
+cp -r --symbolic-link /simple_grid/* /etc/puppetlabs/code/environments/deploy/site/simple_grid/
+cp -r --symbolic-link /simple_grid/* /etc/puppetlabs/code/environments/test/site/simple_grid/
+cp -r --symbolic-link /simple_grid/* /etc/puppetlabs/code/environments/cleanup/site/simple_grid/
 echo "Starting Puppet Server"
 systemctl start puppetserver
 echo "All Set for development"
