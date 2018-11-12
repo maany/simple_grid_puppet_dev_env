806e7a63d3fdbcac156f0a528867663aaf0ad658
maany <imptodefeat@gmail.com>
2018-11-12 10:55:42 +0100

    add r10k into containers

--- config_master/data/init.sh
+++ config_master/data/init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+INSTALL_ENV_DIR=/etc/puppetlabs/code/environments/install
+CONFIG_ENV_DIR=/etc/puppetlabs/code/environments/config
+PRE_DEPLOY_ENV_DIR=/etc/puppetlabs/code/environments/pre_deploy
+DEPLOY_ENV_DIR=/etc/puppetlabs/code/environments/deploy
+TEST_ENV_DIR=/etc/puppetlabs/code/environments/test
+CLEANUP_ENV_DIR=/etc/puppetlabs/code/environments/cleanup
 
 cp /data/puppetserver /etc/sysconfig/puppetserver
 cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
@@ -6,13 +12,29 @@ cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
 hostnamectl set-hostname config_master.cern.ch
 hostnamectl set-hostname config_master.cern.ch
 
-echo "Creating symlinks for puppet module"
-mount --bind /simple_grid /etc/puppetlabs/code/environments/install/site/simple_grid
-mount --bind /simple_grid /etc/puppetlabs/code/environments/config/site/simple_grid
-mount --bind /simple_grid /etc/puppetlabs/code/environments/pre_deploy/site/simple_grid
-mount --bind /simple_grid /etc/puppetlabs/code/environments/deploy/site/simple_grid
-mount --bind /simple_grid /etc/puppetlabs/code/environments/test/site/simple_grid
-mount --bind /simple_grid /etc/puppetlabs/code/environments/cleanup/site/simple_grid
+echo "Creating mount points for puppet module"
+mount --bind /simple_grid $INSTALL_ENV_DIR/site/simple_grid
+mount --bind /simple_grid $CONFIG_ENV_DIR/site/simple_grid
+mount --bind /simple_grid $PRE_DEPLOY_ENV_DIR/site/simple_grid
+mount --bind /simple_grid $DEPLOY_ENV_DIR/deploy/site/simple_grid
+mount --bind /simple_grid $TEST_ENV_DIR/site/simple_grid
+mount --bind /simple_grid $CLEANUP_ENV_DIR/site/simple_grid
+
+
+source /etc/profile.d/rvm.sh
+echo "Downloading dependencies for install environment"
+r10k puppetfile install --puppetfile=$INSTALL_ENV_DIR/Puppetfile
+echo "Downloading dependencies for config environment"
+r10k puppetfile install --puppetfile=$CONFIG_ENV_DIR/Puppetfile
+echo "Downloading dependencies for pre_deploy environment"
+r10k puppetfile install --puppetfile=$PRE_DEPLOY_ENV_DIR/Puppetfile
+echo "Downloading dependencies for deploy environment"
+r10k puppetfile install --puppetfile=$DEPLOY_ENV_DIR/Puppetfile
+echo "Downloading dependencies for test environment"
+r10k puppetfile install --puppetfile=$TEST_ENV_DIR/Puppetfile
+echo "Downloading dependencies for cleanup environment"
+r10k puppetfile install --puppetfile=$CLEANUP_ENV_DIR/Puppetfile
+
 echo "Starting Puppet Server"
 systemctl start puppetserver
 echo "All Set for development"
