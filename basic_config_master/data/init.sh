9bccec200619fc69121c7f7369d29123f943522a
maany <imptodefeat@gmail.com>
2018-12-07 16:36:17 +0100

    upgrade puppet to puppet 5

--- basic_config_master/data/init.sh
+++ basic_config_master/data/init.sh
@@ -5,7 +5,10 @@ cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
 hostnamectl set-hostname basic_config_master.cern.ch
 hostnamectl set-hostname basic_config_master.cern.ch
 
-echo "Creating mount points for puppet module"
+#echo "Building puppet module"
+#puppet module build /simple_grid
+
+echo "Installing puppet module"
 mkdir -p /etc/puppetlabs/code/environments/production/modules/simple_grid
 mount --bind /simple_grid /etc/puppetlabs/code/environments/production/modules/simple_grid
 
