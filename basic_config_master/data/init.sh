8cbcfc32978b9854119526c341410e2c5122f661
maany <imptodefeat@gmail.com>
2018-12-11 10:45:55 +0100

    fix puppet.conf

--- basic_config_master/data/init.sh
+++ basic_config_master/data/init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cp /data/puppetserver /etc/sysconfig/puppetserver
-cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
 
 hostnamectl set-hostname basic_config_master.cern.ch
 hostnamectl set-hostname basic_config_master.cern.ch
