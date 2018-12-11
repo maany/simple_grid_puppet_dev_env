6e489752db3520128688a87c19a3daf1f1e1b68b
maany <imptodefeat@gmail.com>
2018-12-11 16:47:21 +0100

    #34 backup

--- basic_config_master/data/init.sh
+++ basic_config_master/data/init.sh
@@ -11,4 +11,12 @@ echo "Installing puppet module"
 mkdir -p /etc/puppetlabs/code/environments/production/modules/simple_grid
 mount --bind /simple_grid /etc/puppetlabs/code/environments/production/modules/simple_grid
 
+echo "Creating dummy scripts"
+mkdir -p /etc/simple_grid/lifecycle
+touch /etc/simple_grid/lifecycle/wn_pre_config.sh
+touch /etc/simple_grid/lifecycle/wn_pre_inst1.sh
+touch /etc/simple_grid/lifecycle/wn_post_inst1.sh
+touch /etc/simple_grid/lifecycle/ce_pre_config.sh
+touch /etc/simple_grid/lifecycle/ce_pre_inst1.sh
+touch /etc/simple_grid/lifecycle/ce_post_inst1.sh
 echo "All Set for development"
--- lightweight_component/data/init.sh
+++ lightweight_component/data/init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
+#cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
+
 hostnamectl set-hostname lightweight_component01.cern.ch
 hostnamectl set-hostname lightweight_component01.cern.ch
+
+echo "Installing puppet module"
+mkdir -p /etc/puppetlabs/code/environments/production/modules/simple_grid
+mount --bind /simple_grid /etc/puppetlabs/code/environments/production/modules/simple_grid
+
 systemctl restart puppet
-puppet agent -t
+#puppet agent -t
