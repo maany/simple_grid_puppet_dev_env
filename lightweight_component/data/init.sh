1960ae7cc74281c6797aa6313391e298dec13d3f
maany <imptodefeat@gmail.com>
2019-01-10 18:25:11 +0100

    Upgrade to 2 lightweight component containers

--- lightweight_component/data/init.sh
+++ lightweight_component/data/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #cp /data/puppet.conf /etc/puppetlabs/puppet/puppet.conf
 
-hostnamectl set-hostname lightweight_component01.cern.ch
-hostnamectl set-hostname lightweight_component01.cern.ch
+hostnamectl set-hostname $NAME.cern.ch
+hostnamectl set-hostname $NAME.cern.ch
 
 echo "Installing puppet module"
 mkdir -p /etc/puppetlabs/code/environments/production/modules/simple_grid
